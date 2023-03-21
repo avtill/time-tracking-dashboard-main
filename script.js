e2bf646c17e8cb0cda7fb5f29876602834411178
Joanna <joanna.malgorzata@gmail.com>
2023-03-21 14:13:17 +0100

    next try listener

--- script.js
+++ script.js
@@ -4,28 +4,29 @@ fetch('./data.json')
 
 const navDaily = document.getElementById("navDaily");
 const navWeekly = document.getElementById("navWeekly");
-const navMonthly = document.getElementById("navMonthly")
+const navMonthly = document.getElementById("navMonthly");
 
-let workResult = document.getElementById("workResult");
-let workPrev = document.getElementById("workPrev");
-let playResult = document.getElementById("playResult");
-let playPrev = document.getElementById("playPrev");
-let studyResult = document.getElementById("studyResult");
-let studyPrev = document.getElementById("studyPrev");
-let exerciseResult = document.getElementById("exerciseResult");
-let exercisePrev = document.getElementById("exercisePrev");
-let socialResult = document.getElementById("socialResult");
-let socialPrev = document.getElementById("socialPrev");
-let selfcareResult = document.getElementById("selfcareResult");
-let selfcarePrev = document.getElementById("selfcarePrev");
+const workResult = document.getElementById("workResult");
+const workPrev = document.getElementById("workPrev");
+const playResult = document.getElementById("playResult");
+const playPrev = document.getElementById("playPrev");
+const studyResult = document.getElementById("studyResult");
+const studyPrev = document.getElementById("studyPrev");
+const exerciseResult = document.getElementById("exerciseResult");
+const exercisePrev = document.getElementById("exercisePrev");
+const socialResult = document.getElementById("socialResult");
+const socialPrev = document.getElementById("socialPrev");
+const selfcareResult = document.getElementById("selfcareResult");
+const selfcarePrev = document.getElementById("selfcarePrev");
 
-// navDaily.addEventListener("click", ()=>{
-//     console.log("daily")
-// workResult.textContent = `${json[0].timeframes.daily.current}`;
-// workPrev.textContent = `Previous - ${json[0].timeframes.daily.previous}`;
-// });
 
-// navWeekly.addEventListener("click", ()=>{
-//     workResult.textContent = `${json[0].timeframes.weekly.current}`;
-//     workPrev.textContent = "Previous -" + `${json[0].timeframes.weekly.previous}`;
-// });
+navDaily.addEventListener("click", ()=>{
+    console.log("daily")
+    workResult.nodeValue = `${json[0].timeframes.daily.current}`;
+    workPrev.nodeValue = `Previous - ${json[0].timeframes.daily.previous}`;
+});
+
+navWeekly.addEventListener("click", ()=>{
+    workResult.nodeValue = `${json[0].timeframes.weekly.current}`;
+    workPrev.nodeValue = "Previous -" + `${json[0].timeframes.weekly.previous}`;
+});
